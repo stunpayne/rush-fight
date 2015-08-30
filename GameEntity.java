37ef1a67a4f530965c159e2c346e32d8005d70bb
sunny s <sunny.s@flipkarts-MacBook-Pro-2.local>
2015-08-30 12:52:26 +0530

    Creating skeleton classes

---
+++ GameEntity.java
@@ -0,0 +1,11 @@
+package com.example.sunnys.myapplication;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by sunny.s on 27/08/15.
+ */
+public class GameEntity {
+    public Bitmap bmp;
+    public Position pos;
+}
