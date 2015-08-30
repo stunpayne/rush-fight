c070dc9e1e6f4703c887a603f253795bcbfd0df3
sunny s <sunny.s@flipkarts-MacBook-Pro-2.local>
2015-08-30 13:05:30 +0530

    Adding first files

---
+++ app/src/main/java/com/example/sunnys/myapplication/Player.java
@@ -0,0 +1,23 @@
+package com.example.sunnys.myapplication;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by sunny.s on 27/08/15.
+ */
+public class Player extends GameEntity  {
+
+    public Speed speed;
+
+    public Position getPosition()   {
+        return this.pos;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bmp = bitmap;
+    }
+
+    public Speed getSpeed() {
+        return this.speed;
+    }
+}
